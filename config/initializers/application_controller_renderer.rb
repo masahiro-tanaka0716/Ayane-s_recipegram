481cc7e0584edf7f5dbe6d861813704727a67c50
masahiro-tanaka0716 <s1377062@gmail.com>
2021-03-12 09:26:15 +0000

    users imformation (index show edit update)

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8417e0d5d11cdfd8c1cf49fc365ef2bd602d99ff8e1e5c685ba1f8ed2f77b5b239c54b7d5d0b35c3159ea6c20fbb75009828b0d651aa1fd0b431d3f5e05af5b6'
